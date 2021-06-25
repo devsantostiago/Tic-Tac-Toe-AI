57c48668e8da35e42a4f8f9d9e1a648832044209
Tiago Santos <ios.tiagosantos@gmail.com>
2021-06-25 17:38:14 +0200

    Added AI

--- Tic-Tac-Toe-AI/Sources/Tic-Tac-Toe-AI/AI.swift
+++ Tic-Tac-Toe-AI/Sources/Tic-Tac-Toe-AI/AI.swift
@@ -11,5 +11,75 @@ protocol SupportsAI {
 
 class AI {
     
+    let gameEngine: SupportsAI!
+    
+    init(gameEngine: SupportsAI) {
+        self.gameEngine = gameEngine
+    }
+    
+    func getNextMove(board: BoardProtocol, symbol: PlayerSymbol, isAITurn: Bool = true) -> Int {
+        let freeSpaces = board.getFreeSquares()
+        var bestScore = -1
+        var nextMove = -1
+        
+        freeSpaces.forEach {
+            let score = getScoreFor(board: board, AISymbol: symbol, isAITurn: true, nextMove: $0)
+            if score > bestScore {
+                bestScore = score
+                nextMove = $0
+            }
+        }
+        
+        return nextMove
+    }
+    
+    func getScoreFor(board: BoardProtocol, AISymbol: PlayerSymbol, isAITurn: Bool, nextMove: Int) -> Int {
+        let currentBoard = board
+        
+        if isAITurn {
+            let playerSymbol = AISymbol
+            _ = currentBoard.select(square: nextMove, player: playerSymbol)
+        } else {
+            let playerSymbol = AISymbol.getOppositeSymbol()
+            _ = currentBoard.select(square: nextMove, player: playerSymbol)
+        }
+        
+        switch gameEngine.getWinnerWith(board: currentBoard) {
+        case .cross:
+            if isAITurn {
+                if AISymbol == .cross {
+                    return 1
+                }
+                return -1
+            } else {
+                if AISymbol == .cross {
+                    return -1
+                }
+                return 1
+            }
+        case .circle:
+            if isAITurn {
+                if AISymbol == .circle {
+                    return 1
+                }
+                return -1
+            } else {
+                if AISymbol == .circle {
+                    return -1
+                }
+                return 1
+            }
+        case .none:
+            let freeSpaces = currentBoard.getFreeSquares()
+            if freeSpaces.isEmpty {
+                return 0
+            } else {
+                for freeSpace in freeSpaces {
+                    return self.getScoreFor(board: board, AISymbol: AISymbol, isAITurn: !isAITurn, nextMove: freeSpace)
+                }
+            }
+        }
+        return 0
+    }
 }
 
--- Tic-Tac-Toe-AI/Tests/Tic-Tac-Toe-AITests/AITests.swift
+++ Tic-Tac-Toe-AI/Tests/Tic-Tac-Toe-AITests/AITests.swift
@@ -11,4 +11,20 @@ import XCTest
 
 class AITests: XCTestCase {
     
+    func testAISelectsOptimalMoves() {
+        let board = GameTestCases.boardAISymbolAndExpectedChoice.first!.0
+        let symbol = GameTestCases.boardAISymbolAndExpectedChoice.first!.1
+        let optimalChoice = GameTestCases.boardAISymbolAndExpectedChoice.first!.2
+        let aiChoice = getAIChoiceForBoard(boardString: board, symbol: symbol)
+        XCTAssertEqual(aiChoice, optimalChoice)
+    }
+    
+    func getAIChoiceForBoard(boardString: String, symbol: PlayerSymbol) -> Int {
+        let gameEngine = GameEngine()
+        let ai = AI(gameEngine: gameEngine)
+        let boardArray = Helpers.convertStringIntoBoard(boardString)
+        let board = Board(boardArray)
+        return ai.getNextMove(board: board, symbol: symbol)
+    }
+    
 }
